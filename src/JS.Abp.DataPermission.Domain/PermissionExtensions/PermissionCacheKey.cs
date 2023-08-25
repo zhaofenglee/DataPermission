96ce6538c74c316cc1a0498df7dff8a8ad77daf0
zhaofenglee <zhaof_li@163.com>
2023-08-25 19:55:24 +0800

    Change EntityId can be null.

--- src/JS.Abp.DataPermission.Domain/PermissionExtensions/PermissionCacheKey.cs
+++ src/JS.Abp.DataPermission.Domain/PermissionExtensions/PermissionCacheKey.cs
@@ -6,11 +6,11 @@ public class PermissionCacheKey
 {
     public Guid UserId { get; set; }
  
-    public string EntityId { get; set; }
+    public string? EntityId { get; set; }
 
     //构建缓存key
     public override string ToString()
     {
-        return $"{UserId.ToString().ToLower()}_{EntityId.ToString().ToLower()}";
+        return $"{UserId.ToString().ToLower()}_{EntityId?.ToLower()}";
     }
 }
