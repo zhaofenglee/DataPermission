9ae62010fb8d097a123076544d4224541f0f6593
zhaofenglee <zhaof_li@163.com>
2025-06-14 17:26:53 +0800

    Add DataPermission bundling module and contributors for Blazor WebAssembly

---
+++ src/JS.Abp.DataPermission.Blazor.WebAssembly.Bundling/DataPermissionBundleStyleContributor.cs
@@ -0,0 +1,11 @@
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+
+namespace JS.Abp.DataPermission.Blazor.WebAssembly.Bundling;
+
+public class DataPermissionBundleScriptContributor : BundleContributor
+{
+    public override void ConfigureBundle(BundleConfigurationContext context)
+    {
+        // Add your script files here
+    }
+}
